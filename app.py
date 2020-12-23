74383d2b4b8e362256a8b8e3bd48b6053fa9a151
YLGrace <yu-ling,chen>
2020-12-23 13:05:46 +0800

    try to use heroku.Fix PORT.delete reloader.

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ if __name__ == "__main__":
     #port = os.environ.get("PORT", 8000)
     #app.run(host="0.0.0.0", port=port, debug=True)
     PORT = os.environ['PORT']
-    app.run(host="0.0.0.0", port=PORT, debug=True,reloader = True)
+    app.run(host="0.0.0.0", port=PORT, debug=True)
