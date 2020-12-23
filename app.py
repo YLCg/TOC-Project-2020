292e29bf07dc9940d42dac8ec37e2decdd810aa5
YLGrace <yu-ling,chen>
2020-12-23 12:17:15 +0800

    try to use heroku.Fix PORT.

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def show_fsm():
 if __name__ == "__main__":
     #port = os.environ.get("PORT", 8000)
     #app.run(host="0.0.0.0", port=port, debug=True)
-    port = os.environ['PORT']
+    PORT = os.environ['PORT']
     app.run(host="0.0.0.0", port=PORT, debug=True,reloader = True)
